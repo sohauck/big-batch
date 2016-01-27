fbd74b307b322aa95de22f7d1034fa6b9c1456af
sohauck <sofia.hauck@gmail.com>
2016-01-27 17:32:59 +0000

    still building the thing

--- allele-overlap/allele-overlap.pl
+++ allele-overlap/allele-overlap.pl
@@ -18,6 +18,11 @@
 # read the list into hash with keys as isolate names, values as name of list groups
 	# check that only two values exist
 # read the table, isolate by isolate
+	# make an educated guess on which way around the table is?? ^[A-Z]{4}[0-9]{6}
+	# either way, end up with array of arrays
+	# for each array, take first item as locus name, others go as alleles into frequency hash
+		# don't need to be integers
+		# have specific checks for "X", "0" and "I"?
 # only if isolate exists in grouping hash
 # read into locus allele frequency hash for that group 
 # do the usual results from there?
@@ -40,7 +45,7 @@ sub Usage( ; $ );
 my $fTable;
 my $fGroup;
 my $fOut;
-my $skipZero;
+my $skipSym;
 
 # Get Command line options, exits if conditions don't look right
 if( scalar(@ARGV) < 1 ) { Usage("Not enough command line options"); exit; }
@@ -52,44 +57,43 @@ for ($i=0; $i<=$#ARGV; $i++)
 	if($ARGV[$i] eq "-table")		{ $fTable = $ARGV[$i+1] || ''; $arg_cnt++; }
 	if($ARGV[$i] eq "-group")		{ $fGroup = $ARGV[$i+1] || ''; $arg_cnt++; }
 	if($ARGV[$i] eq "-out")			{ $fOut = $ARGV[$i+1] || ''; $arg_cnt++; }
-	if($ARGV[$i] eq "-skipzero")	{ $skipZero = 1 || ''; $arg_cnt++; }
+	if($ARGV[$i] eq "-skipsymbols")	{ $skipSym = 1 || ''; $arg_cnt++; }
 }
 
-# Command line option checks
+# Check that required options were included
 if(! defined $fTable) 	{ Usage("Missing Option: -table <FILE>"); exit; }
 if(! defined $fGroup) 	{ Usage("Missing Option: -group <FILE>"); exit; }
 if(! defined $fOut) 	{ Usage("Missing Option: -out <FILE>"); exit; }
 
-# File checks
+# Check that files exist or don't exist as necessary
 if(! -e $fTable)	{ Usage("Input file does not exist: $fTable"); exit; }
 if(! -e $fGroup)	{ Usage("Input file does not exist: $fGroup"); exit; }
 if(  -e $fOut )		{ Usage("Output file already exists: $fOut"); exit; }
 
 
-# Transponsing if necessary
+# Transponsing if necessary, currently into array of arrays
+
 if($transpose == 1) #!! change to detection?
 {
-	my $t_fIn = $fIn . "-transp"; 
-	my @original = (); #where data will go in the beginning
-	my @transposed = (); #where data will go after being transposed
-	my $columncount = 0; #refers to the final row count and column count, not the initial
-	my $rowcount = 0;
+	my @original = (); my @transposed = ();
+	my $columncount = 0; my $rowcount = 0; # refers to rows and columns of the transposed table, not the original
 	
-
-	open(INFILE, $fIn) or die "Cannot open $fIn\n";
-		while ( my $line = <INFILE> )		
+	open(INTABLE, $fTable) or die "Cannot open $fTable\n";
+		while ( my $line = <INTABLE> )
 		{
-		   chomp($line);
-		   $line =~ s/\r\n?|\n//g; #just in case, removes all other types of line break
-		   $line =~ s/,/\t/g; #if in comma format, switches it to tabs
+		   chomp($line); $line =~ s/\r\n?|\n//g; # clean up line breaks
 	   
-		   my @row = split ('\t', $line);
-		   if ( $rowcount < 2 ) #get rowcount only from first row, since will have no missing values
-			{ $rowcount = @row; }
+		   my @row = split ('\t', $line); # split row by tabs into array of cells
+		   if ( $rowcount < 2 ) { $rowcount = @row; } # transposed row count determined from header of original 
+		
 		   $columncount ++; #add to columncount with each row (will become column)
+		   
 		   push ( @original, [ @row ] );
 		} 
-	close(INFILE);
+	close(INTABLE);
+
+	if ( $columncount == 1 ) { die "You only have one column so your csv is probably not in Unix (LF) format.\n"; }
+
 
 	for my $row (@original) #actually does the transposition
 	{
@@ -98,32 +102,21 @@ if($transpose == 1) #!! change to detection?
 	}
 	
 	@transposed = splice (@transposed, 0, $rowcount); #removes empty rows if more isolates than loci
-
-	open(TRANSP, '>>', $t_fIn) or die "Cannot open $t_In \n"; #opens your results file as appending to it
-		for my $new_row (@transposed) 
-		{
-			for my $new_col (@{$new_row}) 
-			{
-				print TRANSP $new_col, "\t";
-			}
-			print TRANSP "\n";
-		}
-	close(TRANSP);
 }
 
 
 #from this point on, if asks for infile, check if transpose, then use middle file?
 
+open(INTABLE, $fTable) or die "Cannot open $fTable\n";
 
-{ open(INFILE, $fIn) or die "Cannot open $fIn \n"; }
-
-	<INFILE>; # Skips header
-	while ( my $line = <INFILE> ) #goes through each row in the file at a time			
+	<INTABLE>; # Skips header
+	
+	while ( my $line = <INTABLE> ) #goes through each row in the file at a time			
 	{
-		$line =~ s/\r\n?|\n//g; 		#removes line breaks
-		$line =~ s/,/\t/g; 			#switches to tab separators
+		chomp($line); $line =~ s/\r\n?|\n//g; # clean up line breaks
         
-		my @linedata = grep {length()} split(/,/, $line); #put each allele into one item of an array, skips empty cells 
+		# somehow read line into array
+		# my @linedata = grep {length()} split(/,/, $line); #put each allele into one item of an array, skips empty cells 
 
         	my $locus = shift(@linedata); #takes out first array item (locus name) puts it into $locus scalar
 		    		
