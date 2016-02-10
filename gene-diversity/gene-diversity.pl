0e0a47f84d074dc3a1c4e3b954e9ee5b2fa0a1b0
sohauck <sofia.hauck@gmail.com>
2016-02-10 17:32:29 +0000

    working plus examples

--- gene-diversity/gene-diversity.pl
+++ gene-diversity/gene-diversity.pl
@@ -37,24 +37,44 @@ for ($i=0; $i<=$#ARGV; $i++)
 	if($ARGV[$i] eq "-dup")			{ $dup  = $ARGV[$i+1] || ''; $arg_cnt++; }
 }
 
+# Hello!
 print "\n\n\nYou've started the gene diversity script!\n\n";
 my $dbname = "";
 
-# Command line option checks
+# Do we have everything we need to start?
+
+# a table of loci and isolates and whether to transpose it or no
 if(! defined $fTable) 
 { 
 	print "Where is the table with isolates and loci?\n"; 
 	$fTable = <STDIN>; 
 	chomp $fTable; $fTable =~ s/\s+$//; # removes white spaces and line breaks
+	
+	
+	print "\n\nDoes the table have loci as columns? \nIf yes, please enter 'Yes' as the table needs transposing. " .
+	"\nIf no, that is, if the each row of your table is a locus, transposing is not needed, so enter 'No' below.\n"; 
+	$transpose = <STDIN>; 
+	if ( ! $transpose =~ /^[YyNn]/ ) # as long as it starts with a "y" or "n"
+	{ 
+		print "That didn't look like a yes or a no... Try again? \n";
+		$transpose = <STDIN>; 
+	}
+	elsif ( $transpose =~ /^[Yy]/ )
+	{ $transpose = "Yes" }
+	elsif ( $transpose =~ /^[Nn]/ )
+	{ $transpose = "No" }
+	else 
+	{ Usage("Something went wrong with the transposing options"); exit; }
 }
 if(! -e $fTable)  { Usage("Input table file does not exist: $fTable"); exit; }
 
 
+# a directory with FASTA files
 if(! defined $dFAS)  
 { 
 	print 	"\n\nWhere is the directory that includes the FASTA sequences?\n".
 			"Alternatively, enter the name of the sequence database in BIGSDB ".
-			"(for example: pubmlst_chlamydiales_seqdef) for exporting out of the BIGDdb API.\n";
+			"(for example: pubmlst_chlamydiales_seqdef) for exporting out of the BIGSdb API.\n";
 	$dFAS = <STDIN>; 
 	chomp $dFAS; $dFAS =~ s/\s+$//; # removes white spaces and line breaks
 	
@@ -64,26 +84,29 @@ if(! defined $dFAS)
 	{ $dbname = $dFAS; }
 }
 
+# a directory where results can go
 if(! defined $dOut)  
 { 
 	my @tableaddress = split ("/", $fTable);
-	my $tablename = $tableaddress[-1] =~ s/\.[^.]+$//;
+	$tableaddress[-1] =~ s/\.[^.]+$//;
+	$tableaddress[-1] = "GeneDiv-" . $tableaddress[-1];
 	
 	print 	"\n\nName the directory where you'd like the results to go.\n".
 			"If you can't decide, leave this blank and a new folder, in the same place as your ".
-			"table is held will be created, named 'GeneDiv-$tablename'\n";
+			"table is held will be created, named '$tableaddress[-1]'\n";
 	$dOut = <STDIN>;
 
 	if ( $dOut =~ /\w/ )
 	{ if(  -e $dOut)  { Usage("Output directory already exists: $dOut"); exit; } }
 	else 
 	{
-		push (@tableaddress, "GeneDiv-". $tablename . "/" ); 
+		#push (@tableaddress, "/" ); 
 		$dOut = join ("/", @tableaddress); 
 	}
 
 }
 
+# Now that we've asked for everything, let's check that it's all what we expect... 
 print "\n\n\nLet's check if everything is correct before we start...\n\n";
 
 if ( $transpose eq "No" )
@@ -91,65 +114,56 @@ if ( $transpose eq "No" )
 elsif ( $transpose eq "Yes" )
 { print	"Your table of loci as columns and isolates as rows is held at $fTable.\n"  }
 
-if ( $dbname =~ /\w/)
-{ print "FASTA sequences will be downloaded from the BIGSdb API from database $dbname.\n"; }
+
+# including that folder we'll be putting all the results into
+if( -e $dOut) { Usage("Output directory already exists: $dOut"); exit; }
+elsif ( mkdir $dOut )
+{ 
+	print "Your results will be in the new directory $dOut\n"; 
+}
+else { Usage("Could not create output directory: $dOut"); exit; }
+
+if ( $dbname =~ /\w/) # if given database name instead of folder, create that folder now
+{ 
+	print "FASTA sequences will be downloaded from the BIGSdb API from database $dbname.\n"; 
+	
+	if ( mkdir $dOut."/BIGSdb-FASTA/" )
+	{ print "They'll be going into a folder called /BIGSdb-FASTA within your results folder. \n" }
+	else 
+	{ Usage("Could not create FASTA exports folder: $dOut/BIGSdb-FASTA/"); exit; }
+}
 else 
 { print "Your directory of FASTA sequences is at $dFAS.\n" }
 
 
-# Preparing for folder for output
-if( -e $dOut) { Usage("Output directory already exists: $dOut"); exit; }
-else
-{
-	print "Your results will be in the new directory $dOut\n";
-	mkdir $dOut; 
-}
-
-print "Taking alleles from BIGSDB via the API, now up to...\n";
-
-=begin GHOST 
-# Makes output directory and prepares output count file, including header
-
-
-# Transposing, starting with set up...
-print "\nTransposing...";
-my @original = (); #where data will go in the beginning
-my @transposed = (); #where data is moved as its processed
-my $columncount = 0; my $rowcount = 0;
-
-# reads table into the @original array
-open(INFILE, $fTab) or die "Cannot open $fTab\n";
-	while ( my $line = <INFILE> )		
-	{
-		chomp($line); $line =~ s/\r\n?|\n//g; #just in case, removes all other types of line break
-	
-		my @row = split (/\t/, $line); # separates by tabs
-
-		$rowcount = @row;
-		$columncount ++;
-		push ( @original, [ @row ] );
-	} 
-close(INFILE);
-
-if ( $columncount == 1 ) { die "You only have one column so your csv is probably not in Unix (LF) format.\n"; }
+# Give the option to get out if it goes in fact look dodgy
+print "\nLeave blank to continue, enter any letters to exit.\n\n";
+my $confirmation = <STDIN>;
+if ( $confirmation =~ /\w/ )
+{ Usage("You chose to exit: $confirmation"); exit;  }
 
 
 
-# Read in the main table
+# Actually starting the work now
 
+
+# Reading the table in
 my $tableref = ReadTableIn ( $fTable );
 my @aoaTable = @$tableref; #puts table, in array of arrays format, into aoaTable
-if ( $transpose eq "Yes" )
+
+if ( $transpose eq "Yes" ) # transposes the table and puts it back in aoaTable
 {
 	$tableref = TransposeTable ( \@aoaTable );
 	@aoaTable = @$tableref;
 }
 
 
+# Going through table
 
-
-# Copying only relevant loci
-print "Now filter-copying...\n";
+if ( mkdir $dOut."/Observed-FASTA/" )
+{ }
+else 
+{ Usage("Could not create filtered FASTA folder: $dOut/Observed-FASTA/"); exit; }
 
 foreach my $locusrow (@aoaTable) # loop per locus
 {
@@ -157,7 +171,8 @@ foreach my $locusrow (@aoaTable) # loop per locus
 	my %unique_alleles = ();
 	
 	my $locusname = shift(@{$locusrow});
-		
+	$locusname =~ /^(\S+)/; $locusname = $1; # if there are aliases or anything else after the locus ID
+			
 	# populates that hash with unique allele numbers as keys and their number of appearances as values
 	foreach my $allele (@{$locusrow}) #go through each allele in locus
     	{
@@ -168,50 +183,64 @@ foreach my $locusrow (@aoaTable) # loop per locus
     			foreach my $paraallele (@paralogous) #go through each allele in locus
 				{ $unique_alleles{$paraallele}++; } #increase the frequency count	
     		}
-    		    		
-    		else # 
+
+    		else 
     		{ $unique_alleles{$allele}++; } 
 	}
-	
-	my $originalFAS = $dFAS."/".$locusname.".FAS";
-	my $reducedFAS = $dOut."/".$locusname.".FAS";
 
-	if ( open(FULLFASTA, $originalFAS) )
+	# find the file where the original FASTA sequences are
+	my $originalFAS; 
+	
+	if ( $dbname =~ /\w/ )
+	{
+		GetFASTASeqs ( $dbname, $locusname ) ;
+		$originalFAS = $dOut."/BIGSdb-FASTA/".$locusname.".FAS";
+	}
+	else
+	{	
+		$originalFAS = $dFAS."/".$locusname.".FAS";
+	}
+	
+	# name where the file with the reduced number of FASTA sequences will go
+	my $reducedFAS = $dOut."/Observed-FASTA/".$locusname.".FAS";
+
+
+	if ( open(FULLFASTA, $originalFAS) ) # if can actually find the original file
 	{
 		open(REDFASTA, '>', $reducedFAS) or die "Cannot open $reducedFAS\n";
 		{       
 			my $save = 1; # whether to copy the sequence following a >identifier line
-			my $count = 0; # counts how many alleles are copied over, for count-nuc file
+			my $countnuc = 0; # counts how many alleles are copied over
+			
 			while ( my $line = <FULLFASTA> )
 			{
 				if ( $line =~ /^>/ ) # if line is >identifier
 				{
-					chomp $line; 
-					my @alleletitle = split ('_', $line); #if in BIGS BACT01_1 format
+					chomp $line; # ??? COME BACK TO THIS MESS
+					my @alleletitle = split ('_', $line); #if in allele name_1 format
 					my $allelenumber = pop @alleletitle;
 					$allelenumber =~ s/[>]+//g; #in case > is still in there if there is no other ID
 					
 					if ( exists($unique_alleles{$allelenumber}) ) # exists in hash of wanted loci
 					{
-
-						if ($unique_alleles{$allelenumber} >= $dup ) # and in frequence at or above cutoff
+						if ($unique_alleles{$allelenumber} >= $dup ) # and in frequency at or above cutoff
 						{
 							print REDFASTA "\n", $line, "\n";
 							$save = 1;
-							$count ++;
+							$countnuc ++;
 							$unique_alleles{$allelenumber} = 0; # set frequency to 0 as check that was copied
 						}
-						else 
-						{ $save = 0; } # knows to skip the sequences lines that follow unwanted identifiers
-					}
+						else # knows to skip the sequences lines that follow unwanted identifiers below cutoff
+						{ $save = 0; }
+					} 
 					else { $save = 0; }
 				}
 				
 				elsif ( $line =~ /^[A-Z]/ ) # if is a sequence line, copy only is "save" is turned on by wanted identifier
 				{
 					chomp $line; 
-					if ($save == 1)    { print REDFASTA $line; }
-					elsif ($save == 0) { }
+					if ($save == 1)
+					{ print REDFASTA $line; }
 				}	
 			}
 			
@@ -219,7 +248,7 @@ foreach my $locusrow (@aoaTable) # loop per locus
 			
 			if ( exists($unique_alleles{"0"}) ) # move count away from empty if any missing alleles were seen
 			{ $missing = $unique_alleles{"0"}; } # gives the value in the frequency hash when the key is allele "0", the missing allele
-			print UNIQUENUC $locusname . "," . $count . "," . $missing . "\n"; 
+			print  $locusname . "," . $countnuc . "," . $missing . "\n"; 
 
 		}  
 		close(FULLFASTA);
@@ -233,13 +262,14 @@ foreach my $locusrow (@aoaTable) # loop per locus
 		}			
 	}
 	
-	else { # if there isn't a FASTA file to copy over, give warning plus example alleles (might be just "0" which would explain missing file)
+	else # if there isn't a FASTA file to copy over, give warning plus example alleles (might be just "0" which would explain missing file)
+	{ 
 		print "$locusname did not exist as a FASTA file. Alleles in table were..."; 
-		my $count2 = 0; 
+		my $sampleallele = 0; 
 		foreach my $key ( sort keys %unique_alleles ) 
 		{ 
-			if ( $count2 < 5 )
-			{ print " $key"; $count2++; }
+			if ( $sampleallele < 5 )
+			{ print " $key"; $sampleallele++; }
 			else # if already printed 5 allele numbers, just leave it
 			{ print " etc."; last; }
 		 }
@@ -248,7 +278,38 @@ foreach my $locusrow (@aoaTable) # loop per locus
 
 } # closes per-locus loop
 
+=begin GHOST 
 
+
+
+
+
+# Getting the list of loci from the table
+my $headerref = $aoaTable[0]; #dereferences the first line of the table
+my @locuslist = (); 
+
+foreach ( @$headerref ) # loops through each item in first row
+{
+	if ( /^(\S+)/ ) # takes only the first "word", and only if it exists in the %groups hash
+	{ push @locuslist, $1; } # silently add name of group to array as in order
+}
+
+
+
+
+if ( $dbname =~ /\w/)
+{
+	print "We'll start by exporting those FASTA files from $dbname.\n";
+	
+	# first, make and go to the folder where they'll go
+	
+	if ( mkdir "BIGSdb-FASTA/" )
+	{ print "They'll be going in a folder called BIGSdb-FASTA within your results folder. \n" }
+	else 
+	{ Usage("Could not create FASTA exports folder: $dOut/BIGSdb-FASTA/"); exit; }
+	
+	GetFASTASeqs ( $dbname, \@locuslist ) 
+}
 
 
 close(UNIQUENUC);
@@ -318,24 +379,12 @@ sub TransposeTable
 sub GetFASTASeqs
 {
 	my $dbname	= $_[0];
-	my @loci	= @$_[1];
+	my $locus	= $_[1];
 
-	#my $dOut = #??
-	#chdir $dOut;
-	
-	# For each locus in the list, get URL of FASTA file and save it to directory
-	foreach my $locus ( @loci )											
-	{	
-		my $url = "http://rest.pubmlst.org/db/".$dbname."/loci/".$locus."/alleles_fasta";
-	
-		my $file = $locus.".FAS";
-		getstore($url, $file);
-	
-		print "\r$locus";
-	}
-
-	print "Now completed importing FASTA sequences. They are at: $dOut\n";
-
+	my $url = "http://rest.pubmlst.org/db/".$dbname."/loci/".$locus."/alleles_fasta";
+	my $file = $dOut."/BIGSdb-FASTA/".$locus.".FAS";
+		
+	getstore($url, $file);
 }
 
 sub Translate 
@@ -556,19 +605,20 @@ sub Usage( ; $ )
 
 	print << 'EOU';
 
-keepseenloci.pl
+GeneDiv
 
 Description:
 	  
 Usage:
-keepseenloci.pl [ options ]
+gene-diversity.pl [ options ]
 
-Copies only the alleles in FASTA files that appear in a locus/isolate table. 
+Examines the diversity of a set of genomes across tagged loci. 
 
--tin: Table as csv with columns as loci.
--din: Directory with FASTA files where "locusname.FAS" is the file name, like BACT000001.FAS
--dout: Directory where filtered FASTA files will be saved.
--dup: Can set a mininum frequence for allele appearance (default is 1) 
+-tin: Table as tab-separated, with loci and isolates as rows and columns, or reverse if "transpose" option used.
+-transpose: Term included if the table has loci as columns and isolates as rows.  
+-FASTA: Directory with FASTA files where "locusname.FAS" is the file name, like BACT000001.FAS
+-dout: Directory where all results will be saved.
+-dup: Value for the mininum frequency for allele appearance to be included. Default is "1".
 EOU
 
 print "\n\nQuit because: $message\n\n";
