e24d3e1270799f550ab1c441b4cc3bc46a41c257
sohauck <sofia.hauck@gmail.com>
2017-03-31 13:28:18 +0100

    removed merge notes

--- gene-diversity/GbGDiv-shiny/ui.R
+++ gene-diversity/GbGDiv-shiny/ui.R
@@ -3,11 +3,7 @@ library(shiny)
 shinyUI(fluidPage(
   tags$head(includeScript("google-analytics.js")),
   
-<<<<<<< Updated upstream
   titlePanel("GbGDiv Stats & Graphs"),
-=======
-  titlePanel("GbGDiv Stats & Graphs + (Google Analytics!)"),
->>>>>>> Stashed changes
 
   sidebarLayout(
 
@@ -15,28 +11,24 @@ shinyUI(fluidPage(
       
       h4("Upload your GbGDiv table"),
       
-<<<<<<< Updated upstream
       helpText( a("For examples and help, click here.",
                   href="https://github.com/sohauck/GbGDiv",
                   target = "_blank") ),
-=======
->>>>>>> Stashed changes
+
       fileInput('resultstable', 'Choose ResultsTable file',
                 accept=c('text/csv',
                          'text/comma-separated-values,text/plain',
                          '.csv')),
       
       sliderInput("percexc", "Max 'missing' allele tag percentage per locus:", 
-                  min = 0, max = 100, value = 5, step = .1),
+                  min = 0, max = 100, value = 20, step = .1),
 
-     # textOutput("exclusions"), 
       tagAppendAttributes(textOutput("exclusions"), style="white-space:pre-wrap;"),
       
       tags$hr(),
       
       h4("Choose variables to plot"),
       
-<<<<<<< Updated upstream
      conditionalPanel(condition = "output.aligninc == true",
                       selectInput(inputId = "yVar",
                                   label = "Which variable do you want to explore?",
@@ -66,17 +58,6 @@ shinyUI(fluidPage(
                                   selected = "None")      
      ),
      
-=======
-      selectInput(inputId = "yVar",
-                  label = "Which variable do you want to explore?",
-                  choices = c("AllelicDiv","ADivNM","VSitesNuc","VSitesAA","RatioCount","RatioVS"),
-                  selected = "AllelicDiv"),      
-      
-     selectInput(inputId = "colourVar",
-                 label = "Which variable do you want in the point colours?",
-                 choices = c("None (labels only)","Missing","Paralogous","AvgLength"),
-                 selected = "None"),      
->>>>>>> Stashed changes
      
       tags$hr(),
       
@@ -97,39 +78,27 @@ shinyUI(fluidPage(
       # download from Scatter Plot
       conditionalPanel(condition = "input.conditionaltab == 1",
                        downloadButton('downloadPlot', 'Download Plot')
-<<<<<<< Updated upstream
                        ),
      
      conditionalPanel(condition = "input.conditionaltab == 3",
                       downloadButton('downloadTable', 'Download Table')
      )
      
-=======
-                       )
->>>>>>> Stashed changes
 
       
     ), # closes sidebar
 
     mainPanel(
-<<<<<<< Updated upstream
-      # dataTableOutput("data.table")
       tabsetPanel(id = "conditionaltab", type = "tabs",
         tabPanel("Scatter Plot", value = "1",
-=======
-      tabsetPanel(id = "conditionaltab", type = "tabs", 
-        tabPanel("Scatter Plot", value = "1", 
->>>>>>> Stashed changes
+
                  plotOutput(outputId = "mainplot", height = 700,
                             dblclick = "mp_dblclick",
                             brush = brushOpts(
                               id = "mp_brush"))
                  #verbatimTextOutput("brushinfo")
-<<<<<<< Updated upstream
                  ),
-=======
-                 ), 
->>>>>>> Stashed changes
+
         tabPanel("Excluding points", value = "2",
                  helpText("Choose what percentage of isolates a locus can be marked 'missing' (due to no allele designation)",
                           "in and still included in the analysis. In other words, a max 'missing' percentage.",
@@ -138,19 +107,12 @@ shinyUI(fluidPage(
                           "high 'missing' counts (high x-values in the same). The upper plot shows the distribution",
                           "of loci over the range of 0 to the total count of isolates, where the red-dotted line indicated that all",
                           "loci to its right have been excluded from all other graphs and tables in this application."),
-<<<<<<< Updated upstream
+                          
                  plotOutput(outputId = "distplot", height = 400),
                  plotOutput(outputId = "corrplot")),
         tabPanel("Data Table", value = "3",
                  dataTableOutput("data.table"))
       ) # closes tabsetPanel
-=======
-                 plotOutput(outputId = "distplot", height = 400), 
-                 plotOutput(outputId = "corrplot")),
-        tabPanel("Data Table", value = "3", 
-                 dataTableOutput("data.table"))
-       )
->>>>>>> Stashed changes
 
     ) # closes main
   ) # closes layout
